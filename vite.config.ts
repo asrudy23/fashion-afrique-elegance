77025731277c0779d30974ae25bb3893c3af18d6
asrudy23 <ascensionrudy@gmail.com>
2025-07-06 19:42:36 +0100

    Last Change3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/fashion-afrique-elegance/',
   server: {
     host: "::",
     port: 8080,
